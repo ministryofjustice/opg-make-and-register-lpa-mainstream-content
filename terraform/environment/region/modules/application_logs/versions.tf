5a9612d4d914096fbdd5a4ddad5d45013760bb5d
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-11-13 07:56:08 +0000

    update cert update module inputs

--- terraform/environment/region/modules/application_logs/versions.tf
+++ terraform/environment/region/modules/application_logs/versions.tf
@@ -1,16 +1,13 @@
 terraform {
+  required_version = ">= 1.5.2"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.74.0"
+      version = "~> 5.75.0"
       configuration_aliases = [
         aws.region,
       ]
     }
-    local = {
-      source = "hashicorp/local"
-    }
   }
-
-  required_version = "1.9.8"
 }
