38941e1000f3fb184ccb770953c21a8820a9e85c
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2024-11-22 17:29:10 +0000

    create application logs pull in ecr resources

--- terraform/environment/region.tf
+++ terraform/environment/region.tf
@@ -1,3 +1,24 @@
+data "aws_ecr_repository" "make_and_register_lpa_mainstream_content" {
+  name     = "make-and-register-lpa-mainstream-content"
+  provider = aws.management_eu_west_1
+}
+
+data "aws_ecr_image" "make_and_register_lpa_mainstream_content" {
+  repository_name = data.aws_ecr_repository.make_and_register_lpa_mainstream_content.name
+  image_tag       = "latest"
+  provider        = aws.management_eu_west_1
+}
+
+module "allow_list" {
+  source = "git@github.com:ministryofjustice/opg-terraform-aws-moj-ip-allow-list.git?ref=v3.0.2"
+}
+
+
 module "eu_west_1" {
-  source = "./region"
+  source                             = "./region"
+  mrlpa_content_container_sha_digest = data.aws_ecr_image.make_and_register_lpa_mainstream_content.image_digest
+  mrlpa_content_repository_url       = data.aws_ecr_repository.make_and_register_lpa_mainstream_content.repository_url
+  providers = {
+    aws.region = aws.eu_west_1
+  }
 }
